cd2fd93b780bd10f1de512be45ee16576db669d6
madpacket <mads-keini@hotmail.com>
2022-04-26 11:54:27 +0200

    push3

--- src/main/java/dat/startcode/control/CommanderMads.java
+++ src/main/java/dat/startcode/control/CommanderMads.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 public class CommanderMads extends Command{
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws DatabaseException {
+        int x = 10;
+        x += 10;
         return "commanderMads.jsp";
     }
 }
